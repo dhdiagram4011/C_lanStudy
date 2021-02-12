54df9144378242766ebdb960226e215eb21dcf9c
dhdiagram4011 <dhdiagram@gmail.com>
2021-02-12 11:48:16 +0900

    exam and learning 20210212

---
+++ first/exam2-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 12);
+    printf("%d\n", 014);
+    printf("%d\n", 0xc);
+
+    //8진수 , 16진수-소문자, 16진수-대문자
+
+    printf("%o\n", 12);
+    printf("%x\n", 12);
+    printf("%X\n", 12);
+
+
+    return 0;
+}
+
---
+++ first/exam2-7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("학번 : %d\n", 32165);
+    printf("이름 : %s\n", "홍길동");
+    printf("학점 : %c\n", 'A');
+
+    return 0;
+}
+
+// int main(void)
+// {
+//     printf("%c\n", 'A');
+//     printf("%s\n", "A"); 
+//     printf("%c 은 %s 입니다.\n", '1', "first");
+
+//     return 0;
+// }
+
+// TEST
+
+
